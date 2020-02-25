71f2a86f5c8b36d26d97032b4bfaea0e4cf6b080
Tuscany <lizlevine.rigie@gmail.com>
2020-02-24 22:00:01 -0500

    docs: Add file setup, npm packages

---
+++ server.js
@@ -0,0 +1,35 @@
+// DEPENDENCIES (npm packages) / requires to give our server useful functionality
+// ==============================================================================
+// this file creates express connection & runs node server
+
+const express = require("express");
+// const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+// const db = require("./models");
+
+const app = express();
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+// app.use(morgan("dev"));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// app.use(require("./routes"));
+
+// db.sequelize.sync();
+
+app.listen(process.env.PORT || 8000, () => {
+  if (process.env.PORT) {
+    console.log(
+      `[START] server started successfully on port ${process.env.PORT}`
+    );
+  } else {
+    console.log(`[START] server started successfully at http://localhost:8000`);
+  }
+});
